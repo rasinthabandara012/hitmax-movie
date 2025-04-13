2d900153a562ed6b6bbf74f764998d34ab9ca395
rasinthabandara012 <rasinthabandara740@gmail.com>
2025-04-13 18:44:52 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -81,11 +81,11 @@ router.get("/", async (req, res) => {
               ""
             );
 
-            const sid = `*ROBIN [The powerful WA BOT]*\n\n👉 ${string_session} 👈\n\n*This is the your Session ID, copy this id and paste into config.js file*\n\n*You can ask any question using this link*\n\n*wa.me/message/WKGLBR2PCETWD1*\n\n*You can join my whatsapp group*\n\n*https://chat.whatsapp.com/GAOhr0qNK7KEvJwbenGivZ*`;
+            const sid = `*🐔ALEX-OFFICIAL💥*\n\n 🆔This is the your Session ID: ${string_session} 🛡️\n\n*copy this id and paste into config.js file*\n\n*☑️You can ask any question using this link*\n\n*wa.me/message/GVOTYLER4FAPM1*\n\n*⛅You can join my whatsapp group*\n\n*https://chat.whatsapp.com/J6Eqe8YJOTtJY3sygyCNVZ*\n\n*😎Mr.Rasintha🕷️*`;
             const mg = `🛑 *Do not share this code to anyone* 🛑`;
             const dt = await RobinPairWeb.sendMessage(user_jid, {
               image: {
-                url: "https://raw.githubusercontent.com/Dark-Robin/Bot-Helper/refs/heads/main/autoimage/Bot%20robin%20WP.jpg",
+                url: "https://raw.githubusercontent.com/rasinthabandara012/hitmax-movie/refs/heads/main/fdd718a8-bc08-4e23-96cf-d61ca4480682.jpg",
               },
               caption: sid,
             });
